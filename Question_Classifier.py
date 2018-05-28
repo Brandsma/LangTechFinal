fecb79e692821481fe61354df1fbe42cdbc74636
A. Brandsma <s2889439@fwn-bborg-edu-74-122.fmnsedu.rug.nl>
2018-05-28 15:43:26 +0200

    Added a question classifier

---
+++ Question_Classifier.py
@@ -0,0 +1,56 @@
+# Find the Root
+# Look at Dependencies of Root
+# Find {'nsubj', 'dobj', 'prep', 'attr', 'ccomp'} (Question words can also be attributes)
+# With those dependencies find the subject and object
+# What's type of the question
+	#Does root have a nsubj:
+		# NO: 
+			# list/show/print/ hwatadffaheve
+		# YES:
+			# iS 'is' the root the first word in the snetences?
+				#YES: yes/no
+				#NO:
+					# Do you find "how many" "how much"
+						# YES:
+							#Count question
+						# NO:
+							# Do you find "which" "who" "what" "How" etc.
+								#YES:
+									#Which question
+								#NO:
+									# Did you find the dep acomp("How big is china?")
+										#YES:
+											# How big question (How far above sea level)
+										#NO:
+											# Handle Special cases (could you give me the population of the Netherlands?)
+											# Otherwise raise error
+from enum import Enum
+class QuestionTypes(Enum):
+	whichQuestion = 1
+	countQuestion = 2
+	listQuestion = 3
+	YesNoQuestion = 4
+	HowBigQuestion = 5
+
+def isCountQuestion(question):
+	return question.find("how many") != -1 or question.find("how much") != -1
+
+def isWhichQuestion(question):
+	return question.find("which") != -1 or question.find("who") != -1 or question.find("what") != -1 or question.find("how") != -1
+
+def classifyQuestion(question, nlp):
+	parsedQuestion = nlp(question)
+	root = findRoot(parsedQuestion)
+	if not hasDependency(root, "nsubj"):
+		return QuestionTypes.listQuestion
+	elif root == parsedQuestion[0].text:
+		return QuestionTypes.YesNoQuestion
+	elif isCountQuestion(question):
+		return QuestionTypes.countQuestion
+	elif isWhichQuestion(question):
+		return QuestionTypes.whichQuestion
+	elif hasDependency(root, "acomp"):
+		return QuestionTypes.HowBigQuestion
+	else:
+		raise ValueError
+		
---
+++ main.py
@@ -0,0 +1,43 @@
+# Types of questions
+# Pattern match on the dependecies
+
+# Function to solve What is X of Y question
+# Find the root
+# Verb
+
+
+# 			Question classifier
+# Find the dependecies of Root
+# { 'nsubj', 'attr', 'dobj', 'dep', ,'prep', 'pobj', 'compound' }
+
+
+
+# What/Who is/are the X of Y
+# List-questions
+# Yes/No Questions
+# Count questions
+# (Highest/Deepest Questions)
+# (Questions with qualified statements)
+
+# 			Function for each type of question
+
+# 			Query Creator
+
+# 			Query Fire
+from Question_Classifier import classifyQuestion
+
+switch = {
+	whichQuestion: whichQuestionFunction,
+	listQuestion: listQuestionFunction
+}
+
+if __main__ == "__main__":
+	# Input
+	# Question Classifier
+	QuestionType = classifyQuestion()
+	# Switch based on question type
+		# Function for each question
+		# Each question returns an answer
+	answer = switch[QuestionType](question)
+	print answer
+		
